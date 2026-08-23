dd3a08ae8925e95bdb51b138b9855aec8c83e089
Muhammad Ibraheem <codewithibf@gmail.com>
2026-08-23 17:34:47 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import AdminContactDashboard from "./pages/AdminContactDashboard";  // ← add this
+import AdminContactDashboard from "./pages/AdminContactDashboard";
 
 const queryClient = new QueryClient();
 
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/admin" element={<AdminContactDashboard />} />  {/* ← add this */}
+          <Route path="/admin" element={<AdminContactDashboard />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
